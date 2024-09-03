3d123defe76f3b92275f4e2ce5ae3aa38856bb71
Martin Donadieu <martindonadieu@gmail.com>
2024-09-03 13:47:57 +0100

    feat: add definition

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,32 @@
+import type { PluginListenerHandle } from "@capacitor/core";
+
+export interface uploadOption {
+  id: string;
+  filePath: string;
+  serverUrl: string;
+  notificationTitle: number;
+  headers: {
+    [key: string]: string;
+  }
+}
+export interface UploadEvent {
+  /**
+   * Current status of upload, between 0 and 100.
+   *
+   */
+  name: 'uploading' | 'completed' | 'failed';
+  payload: {
+    percent?: number;
+    error?: string; 
+  }
+  id: string;
+}
+
 export interface UploaderPlugin {
-  echo(options: { value: string }): Promise<{ value: string }>;
+  startUpload(options: uploadOption): Promise<{ value: string }>;
+  removeUpload(options: { id: string }): Promise<void>;
+  addListener(
+    eventName: "events",
+    listenerFunc: (state: UploadEvent) => void,
+  ): Promise<PluginListenerHandle>;
 }
