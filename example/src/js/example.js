439845f63b1fd29daa615662b6e07593dba85b9c
Martin Donadieu <martindonadieu@gmail.com>
2025-10-15 18:06:33 +0100

    fix: lint config

--- example/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { Uploader } from '@capgo&#x2F;capacitor-uploader';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    Uploader.echo({ value: inputValue })
-}
